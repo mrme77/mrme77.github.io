0daee9233142ac75ab3c9b48b2f629dce941ba5b
Pasquale <137796364+mrme77@users.noreply.github.com>
2025-07-16 12:31:27 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+function showTab(tabId) {
+  const tabs = document.querySelectorAll('.tab');
+  tabs.forEach(tab => {
+    tab.classList.remove('active');
+  });
+  document.getElementById(tabId).classList.add('active');
+}
